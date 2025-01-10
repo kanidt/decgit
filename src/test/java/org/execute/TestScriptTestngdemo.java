da6e3fd483db8b3b5f7debfb5b9981a345d0b606
unknown <arumugakani231991@gmail.com>
2025-01-10 08:27:57 -0800

    message

---
+++ src/test/java/org/execute/TestScriptTestngdemo.java
@@ -0,0 +1,22 @@
+package org.execute;
+import org.testng.annotations.Test;
+
+public class TestScriptTestngdemo {
+	@Test
+	private void tc01() {
+		System.out.println("Test1");
+	}
+	@Test
+	private void tc02() {
+		
+		System.out.println("Test2");
+	}
+	@Test
+	private void tc03() {
+		System.out.println("Test3");
+	}
+	@Test
+   private void tc04() {
+	   System.out.println("Test4");
+   }
+}
